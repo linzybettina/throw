9ad29ff4f3f12c7080e826f7361075c26e583348
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-13 23:06:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace throwkeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Ourmoney = OurMoney(10, -10);
+
+        }
+        public static int OurMoney(int mymoney, int yourmoney)
+        {
+            if (mymoney < 0 || yourmoney < 0) throw new ArithmeticException("my money or your money is not valid");
+            return mymoney + yourmoney;
+        }
+                
+    }
+}
